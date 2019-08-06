d3d9e12bd20b498ad886ca725b2b29b4edba49a7
Patrick Heflin <patrickslx@yahoo.com>
2019-08-06 10:20:21 -0500

    add static path

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express()
 app.use(cors())
 app.use(morgan('tiny'))
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 function getResults(body) {
   const $ = cheerio.load(body)
   const table = $('table#team_batting tr')
